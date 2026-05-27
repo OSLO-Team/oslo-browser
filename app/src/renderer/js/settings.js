7fb0ffa879e16dc5f05b7dd0f2d6a8633789abac
Emir Can <emircanturan16@users.noreply.github.com>
2026-05-27 14:13:01 +0300

    bump: version 1.0.7

--- app/src/renderer/js/settings.js
+++ app/src/renderer/js/settings.js
@@ -1046,11 +1046,11 @@ export function initSettings() {
 
   const versionDisplay = document.getElementById('about-version-display');
   if (versionDisplay) {
-    versionDisplay.textContent = '1.0.6';
+    versionDisplay.textContent = '1.0.7';
   }
   const versionDisplayMain = document.getElementById('about-version-display-main');
   if (versionDisplayMain) {
-    versionDisplayMain.textContent = '1.0.6';
+    versionDisplayMain.textContent = '1.0.7';
   }
 
   if (navHome) {
